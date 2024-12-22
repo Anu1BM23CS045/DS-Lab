0a80c31686f5f863f7de72d2645cc7a914a9640f
Anu1BM23CS045 <anusai.cs23@bmsce.ac.in>
2024-12-22 21:09:55 +0530

    Update dfs.c

--- Lab10/dfs.c
+++ Lab10/dfs.c
@@ -1,48 +1,52 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int a[10][10],vis[25],i,j,n,v,flag=1;
+int n,a[10][10],vis[25];
 
 void dsf(int);
 
 void main()
 {
+    int i,j,count=0;
     printf("Enter number of vertices:");
     scanf("%d",&n);
+    for(i=1;i<=n;i++){
+    vis[i]=0;
+    }
     printf("Enter adjacency matrix:\n");
     for(i=1;i<=n;i++)
     {
         for(j=1;j<=n;j++)
-        scanf("%d",&a[i][j]);
-    }
-    for(i=1;i<=n;i++)
-    vis[i]=0;
-    printf("Traversal from 1:");
-    for(i=1;i<=n;i++)
-    {
-        if(vis[i]==0)
-            dsf(i);
-    }
-    for(i=1;i<=n;i++)
-    {
-        if(vis[i]==0)
         {
-            printf("\nThe Graph is disconnected");
-            return;
+            scanf("%d",&a[i][j]);
         }
     }
-    printf("\nThe graph is connected");
+    
+    printf("Traversal from vertex 1:\n");
+    dsf(1);
+    for(i=1;i<=n;i++)
+    {
+        if(vis[i]==1)
+         count++;
+    }
+    if(count==n)
+         printf("\n The graph is Connected ");
+    else
+        printf("\n The graph is not Connected ");     
 
+    
 }
 
 void dsf(int v)
-{
-    printf("%d->",v);
+{   
+    int i;
     vis[v]=1;
     for(i=1;i<=n;i++)
-    {
         if(a[v][i]==1 && vis[i]==0)
-            dsf(i);
-    }
+        {
+          printf("%d->%d\n",v,i); 
+          dsf(i);      
+        }
+        
 }
 
